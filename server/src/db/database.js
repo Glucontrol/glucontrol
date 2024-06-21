3d7f2badad80c72b71a0697f1ddaf717d05ba81e
unknown <ezequielafeita@gmail.com>
2024-06-21 17:39:39 -0300

    Ordenamiento de las carpetas

---
+++ server/src/db/database.js
@@ -0,0 +1,19 @@
+// Requerimos mysql.
+const mysql = require('mysql2/promise');
+
+// Creamos una funcion para realizar la conexion a la bd.
+const connectDB = async ()=> {
+    return await mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'glucontrol'
+    })
+
+    
+}
+
+// Exportamos la funcion para realizar la conexion desde cualquier archivo.
+module.exports = {
+    connectDB
+}
