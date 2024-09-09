066708d7121b720584d4bae05aa5d5af05a0d07b
Zique <ezequielafeita@gmail.com>
2024-09-08 22:00:36 -0300

    Intento de landing

--- nuevo client/src/components/Header.jsx
+++ nuevo client/src/components/Header.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 export const Header = () =>{
     return(
-        <header>
+        <header className="sticky top-0">
             <nav>
-                <ul className=" grid grid-cols-4 text-center h-12 bg-slate-700 text-gray-200">
-                    <li className="table-cell hover:scale-110">Hola</li>
-                    <li className="table-cell hover:scale-110">Artículos</li>
-                    <li className="table-cell hover:scale-110">Registros</li>
-                    <li className="table-cell align-sub hover:scale-110">Nosotros</li>
-
+                <ul className=" grid grid-cols-4 text-center h-12 bg-slate-700 text-gray-200 text-xl">
+                    <li className="flex justify-center mt-3 hover:scale-110 hover:text-yellow-200">Hola</li>
+                    <li className="flex justify-center mt-3 hover:scale-110 hover:text-yellow-200">Artículos</li>
+                    <li className="flex justify-center mt-3 hover:scale-110 hover:text-yellow-200">Registros</li>
+                    <li className="flex justify-center mt-3 hover:scale-110 hover:text-yellow-200">Nosotros</li>
                 </ul>
             </nav>
         </header>
