40943eab47908f59b7df7bcf7a2ac97b9b8dca69
esquivel mariano <esquivelmariano@18gmail.com>
2024-10-10 15:53:07 -0300

    se agrego el boton de crear articulos

--- nuevo client/src/views/Articulos.jsx
+++ nuevo client/src/views/Articulos.jsx
@@ -15,9 +15,20 @@ export const Articulos = () => {
     <>
       <main className="flex">
         <Navbar />
-        <div className="container flex">
-          <div className="flex flex-wrap flex-row justify-around gap-4">
-            {Tarjetas}
+        <div className="container flex items-center justify-center">
+          <div className="flex flex-col items-center">
+            <button
+              className="h-20 w-40 shadow-md rounded-2xl self-center text-2xl hover:shadow-2xl 
+          hover:scale-110 hover:bg-gray-200 duration-200 mt-10 mb-10"
+              onClick={() => {
+                window.location.href = "/crearArticulo";
+              }}
+            >
+              Crear Articulo
+            </button>
+            <div className="flex flex-wrap flex-row justify-around gap-4 mt-10">
+              {Tarjetas}
+            </div>
           </div>
         </div>
       </main>
