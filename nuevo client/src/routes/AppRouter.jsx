e639b50b089b4c2b0e0db856c6ef5e0404faa0ac
Tatiana Medina <tatymedina284@gmail.com>
2024-10-17 15:34:52 -0300

    se agrego el form para registrar glucosa

--- nuevo client/src/routes/AppRouter.jsx
+++ nuevo client/src/routes/AppRouter.jsx
@@ -25,7 +25,7 @@ const AppRouter = () => {
         <Route path="/me" element={<User />}></Route>
         <Route path="/editProfile" element={<EditDiabeticProfile />}></Route>
         <Route path="/home" element={<Home />}></Route>
-        <Route path="/crearArticulo" element={<CrearArticulo />}></Route>
+      
         <Route path="/crearRegistro" element={<CrearRegistro />}></Route>
         <Route path="/Registros" element={<Registros />}></Route>
       </Routes>
--- nuevo client/src/views/crearArticulo.jsx
+++ nuevo client/src/views/crearArticulo.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { BiArrowBack } from "react-icons/bi";
-import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css";
+
 import "../style.css";
 
 const CrearArticulo = () => {
