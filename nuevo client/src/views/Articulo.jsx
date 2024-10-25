0468caac527b484b16273c5ea9f9bb8e857d0e15
Tatiana Medina <tatymedina284@gmail.com>
2024-10-25 16:09:15 -0300

    los articulos ya se guardan con saltos de linea

--- nuevo client/src/views/Articulo.jsx
+++ nuevo client/src/views/Articulo.jsx
@@ -1,26 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Header } from "../components/Header.jsx";
-import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { link } from "../utilities/functions.js";
 import { LuChevronsRight } from "react-icons/lu";
+import { BiArrowBack } from "react-icons/bi";
 
 export const Articulo = () => {
   const url = useLocation().search.slice(1);
-  const [contenido, setContenido] = useState(22);
+  const [contenido, setContenido] = useState({});
 
   useEffect(() => {
     link.articuloId(url).then((response) => setContenido(response));
-    console.log(contenido);
-  }, []);
+  }, [url]);
 
   return (
     <>
       <main className="min-h-screen bg-gray-50">
         <div className="mx-auto max-w-4xl p-6 bg-white shadow-lg rounded-lg my-12">
-          <div className="flex items-center space-x-2 text-gray-600">
-            <p className="text-lg font-semibold">{contenido.Categoria}</p>
-            <LuChevronsRight className="h-6 w-6" />
+          <div className="flex items-center mb-6">
+            <a href="/articulos" className="text-blue-500 hover:text-blue-700">
+              <BiArrowBack size={24} />
+            </a>
+            <h1 className="text-2xl font-bold ml-4">Articulos</h1>
           </div>
 
           <h1 className="text-center text-4xl font-bold text-gray-800 mt-4 mb-6">
@@ -31,9 +32,12 @@ export const Articulo = () => {
             Por <span className="font-medium">{contenido.Autor}</span>
           </p>
 
-          <div className="prose max-w-none prose-lg text-gray-700 mx-auto leading-relaxed">
-            <p>{contenido.Contenido}</p>
-          </div>
+          <div
+            className="prose max-w-none prose-lg text-gray-700 mx-auto leading-relaxed h-ful"
+            dangerouslySetInnerHTML={{
+              __html: contenido.Contenido?.replace(/\n/g, "<br>"),
+            }}
+          />
         </div>
       </main>
     </>
--- nuevo client/src/views/crearArticulo.jsx
+++ nuevo client/src/views/crearArticulo.jsx
@@ -1,41 +1,125 @@
-import React from "react";
-import { Header } from "../components/Header.jsx";
-import { useState, useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import React, { useState } from "react";
+import { BiArrowBack } from "react-icons/bi";
+import "../style.css";
 import { link } from "../utilities/functions.js";
-import { LuChevronsRight } from "react-icons/lu";
+import toast, { Toaster } from "react-hot-toast";
 
-export const CrearArticulo = () => {
-  const url = useLocation().search.slice(1);
-  const [contenido, setContenido] = useState(22);
+const CrearArticulo = () => {
+  const [contenido, setContenido] = useState("");
+  const [nombreArticulo, setNombreArticulo] = useState("");
+  const [categoria, setCategoria] = useState("");
+  const [error, setError] = useState("");
 
-  useEffect(() => {
-    link.articuloId(url).then((response) => setContenido(response));
-    console.log(contenido);
-  }, []);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Validar que todos los campos obligatorios estén llenos
+    if (!nombreArticulo || !categoria || !contenido) {
+      setError("Por favor, completa todos los campos obligatorios.");
+
+      return;
+    }
+
+    setError(""); // Reiniciar el error si todo está correcto
+
+    const Fecha = new Date().toISOString();
+
+    const data = {
+      Titulo: nombreArticulo,
+      Categoria: categoria,
+      Contenido: contenido,
+      Fecha: Fecha,
+    };
+
+    try {
+      await link.createArticulo(data);
+      toast.success("Artículo creado exitosamente!"); // Notificación de éxito
+
+      setTimeout(() => {
+        window.location.href = "/articulos";
+      }, 1000);
+    } catch (err) {
+      toast.error("Hubo un error al crear el artículo."); // Notificación de error
+    }
+  };
 
   return (
-    <>
-      <main className="min-h-screen bg-gray-50">
-        <div className="mx-auto max-w-4xl p-6 bg-white shadow-lg rounded-lg my-12">
-          <div className="flex items-center space-x-2 text-gray-600">
-            <p className="text-lg font-semibold">{contenido.Categoria}</p>
-            <LuChevronsRight className="h-6 w-6" />
+    <main className="max-w-4xl mx-auto p-4">
+      <Toaster />
+      <div className="flex items-center mb-6">
+        <a
+          href="/articulos"
+          className="text-blue-500 hover:text-blue-700 cursor-pointer"
+        >
+          <BiArrowBack size={24} />
+        </a>
+        <h1 className="text-2xl font-bold ml-4">Crear Nuevo Artículo</h1>
+      </div>
+      <div>
+        <h2 className="text-xl mb-4">
+          Completa con los detalles de tu artículo
+        </h2>
+        {error && <p className="text-red-500 mb-4">{error}</p>}{" "}
+        {/* Mostrar mensaje de error */}
+        <form className="space-y-6" onSubmit={handleSubmit}>
+          <div className="flex flex-col">
+            <label htmlFor="nombreArticulo" className="mb-2 font-semibold">
+              Nombre del Artículo
+            </label>
+            <input
+              type="text"
+              id="nombreArticulo"
+              name="nombreArticulo"
+              value={nombreArticulo}
+              onChange={(e) => setNombreArticulo(e.target.value)}
+              className="p-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              placeholder="Introduce el nombre del artículo"
+            />
           </div>
-
-          <h1 className="text-center text-4xl font-bold text-gray-800 mt-4 mb-6">
-            {contenido.Titulo}
-          </h1>
-
-          <p className="text-center text-sm text-gray-500 mb-8">
-            Por <span className="font-medium">{contenido.Autor}</span>
-          </p>
-
-          <div className="prose max-w-none prose-lg text-gray-700 mx-auto leading-relaxed whitespace-pre-line">
-            <p>{contenido.Contenido}</p>
+          <div className="flex flex-col">
+            <label htmlFor="categoriaArticulo" className="mb-2 font-semibold">
+              Categoría
+            </label>
+            <select
+              id="categoriaArticulo"
+              className="select w-full rounded-lg focus:outline-none p-2 border border-gray-300 focus:ring-2 focus:ring-blue-500"
+              value={categoria}
+              onChange={(e) => setCategoria(e.target.value)}
+            >
+              <option value="" disabled>
+                Selecciona una categoría
+              </option>
+              <option>Salud</option>
+              <option>Nutrición</option>
+              <option>Ejercicio</option>
+              <option>Tecnología</option>
+              <option>Investigación</option>
+              <option>Consejos</option>
+              <option>Recetas</option>
+            </select>
           </div>
-        </div>
-      </main>
-    </>
+          <div>
+            <label htmlFor="contenidoArticulo" className="mb-2 font-semibold">
+              Contenido
+            </label>
+            <textarea
+              id="contenidoArticulo"
+              value={contenido}
+              onChange={(e) => setContenido(e.target.value)}
+              placeholder="Escribe el contenido del artículo..."
+              className="w-full h-64 p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+          <button
+            type="submit"
+            className="bg-blue-400 text-white w-1/3 mt-16 p-2 rounded-lg hover:bg-blue-600 hover:text-white"
+          >
+            Listo
+          </button>
+        </form>
+      </div>
+    </main>
   );
 };
+
+export { CrearArticulo };
