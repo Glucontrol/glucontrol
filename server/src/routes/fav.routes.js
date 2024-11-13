1690338a21bcbe15815cc59e1e758b01e1b81145
Tatiana Medina <tatymedina284@gmail.com>
2024-11-13 16:29:27 -0300

    se arregló el nombre de un archivo

--- server/src/routes/fav.routes.js
+++ server/src/routes/fav.routes.js
@@ -2,14 +2,13 @@ import { Router } from "express";
 import {
   agregarFavorito,
   obtenerEstadoFavorito,
-  listarFavoritos
-} from "../controllers/fav.controllers.js";
+  listarFavoritos,
+} from "../controllers/fav.controller.js";
 
 const router = Router();
 
 router.post("/favoritos", agregarFavorito);
 router.get("/favoritos/:articleId", obtenerEstadoFavorito);
 router.get("/favoritos", listarFavoritos);
-
 
 export default router;
