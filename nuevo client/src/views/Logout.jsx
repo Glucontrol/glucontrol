b19d045c0ef7cb0981d10a503291f32f786a4fcb
esquivel mariano <esquivelmariano@18gmail.com>
2024-11-15 16:39:21 -0300

    guardar

--- nuevo client/src/views/Logout.jsx
+++ nuevo client/src/views/Logout.jsx
@@ -1,16 +1,11 @@
-import { useEffect } from "react";
+import React from "react";
 import { link } from "../utilities/functions";
-import { useNavigate } from "react-router-dom";
-
 const LogOut = () => {
   link.logOut();
-
   return (
-    <main className="flex flex-col items-center space-y-4">
-      <h1>Nos vemos pronto</h1>
-      <p>Redirigiendo al inicio en 3 segundos...</p>
-    </main>
+    <>
+      <h1>Cerrar Sesión</h1>
+    </>
   );
 };
-
 export default LogOut;
