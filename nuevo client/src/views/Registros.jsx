e6190a738881609d68ca713442e70ca11d85fb10
unknown <ezequielafeita@gmail.com>
2024-11-19 10:47:40 -0300

    Chartjs

--- nuevo client/src/views/Registros.jsx
+++ nuevo client/src/views/Registros.jsx
@@ -2,26 +2,7 @@ import React, { useEffect, useState } from "react";
 import { link } from "../utilities/functions";
 import { Navbar } from "../components/Navbar";
 import Calendar from "../components/Calendar";
-
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  LineElement,
-  PointElement,
-  LineController,
-  Title,
-} from "chart.js";
-
-ChartJS.register(
-  CategoryScale,
-  LineController,
-  LineElement,
-  PointElement,
-  LinearScale,
-  Title
-);
-import { Line } from "react-chartjs-2";
+import Chart from "../components/Chart";
 
 import {
   LuSyringe,
@@ -77,24 +58,9 @@ export const Registros = () => {
               Registrar
             </button>
           </div>
-
-          <Line
-            data={{
-              labels: ["Enero", "Febrero", "Marzo", "Abril"],
-              datasets: [
-                {
-                  label: "Ventas",
-                  data: [12, 19, 3, 5],
-                  borderColor: "rgba(255, 99, 132, 1)",
-                  backgroundColor: "rgba(255, 99, 132, 0.2)",
-                  pointBackgroundColor: "rgba(255, 99, 132, 1)",
-                  pointBorderColor: "rgba(255, 99, 132, 1)",
-                  pointHoverBackgroundColor: "rgba(255, 99, 132, 1)",
-                  pointHoverBorderColor: "rgba(255, 99, 132, 1)",
-                },
-              ],
-            }}
-          />
+          <div className="w-1/2 mx-auto">
+            <Chart />
+          </div>
           <Calendar props={month} onClick={setMonth} />
           {month}
 
