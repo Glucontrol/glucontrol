58400542ce8148211cd640368249e454d04edaa7
Zique <ezequielafeita@gmail.com>
2025-01-03 22:54:09 -0300

    Cookie

--- server/src/middlewares/getUser.js
+++ server/src/middlewares/getUser.js
@@ -2,14 +2,19 @@ import jwt from "jsonwebtoken";
 import { client } from "../db/database.js";
 import { generarOID } from "../helpers/generarOID.js";
 
-const verifyUser = async (req, res, next) => {
-  const { cookie } = req.headers;
-  let token = "";
+const getCookie = (cookie) => {
   if (cookie) {
-    token = cookie.includes(";") ? cookie.split(";")[0] : cookie.substr(6);
-    console.log(token);
+    const cookies = cookie.includes(";") ? cookie.split(";") : cookie.substr(6);
+    const token = cookies.find((el)=>el.includes("token") ? el : false)
+    return token.substr(7)
   }
 
+}
+
+const verifyUser = async (req, res, next) => {
+  const { cookie } = req.headers;
+  const token = getCookie(cookie)
+  console.log(token)
   try {
     generarOID(jwt.verify(token, "mysecret").id)
       .then((o_id) =>
